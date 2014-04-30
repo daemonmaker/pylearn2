86fed59467d13037b1951f04c40003660462c5b5
Dustin Webb <daemonmaker@gmail.com>
2014-04-30 11:04:55 -0400

    Updated training object and SGD to cache and resuse the sgd_update function.

--- pylearn2/train.py
+++ pylearn2/train.py
@@ -22,6 +22,8 @@ log = logging.getLogger(__name__)
 
 
 class Train(object):
+    update_func = None
+
     """
     A class representing the main loop of the training script.  Trains the
     specified model using the specified algorithm on the specified dataset.
@@ -162,6 +164,17 @@ class Train(object):
                 if not continue_learning:
                     break
         else:
+            self.update_func = self.algorithm.setup(
+                model=self.model,
+                dataset=self.dataset,
+                update_func=self.update_func
+            )
+            self.setup_extensions()
+            # Model.censor_updates is used by the training algorithm to
+            # enforce constraints after each step of learning. Here we
+            # make sure the constraints are enforced from the start.
+            self.model.enforce_constraints()
+
             if not hasattr(self.model, 'monitor'):
                 # TODO: is this really necessary? I just put this error here
                 # to prevent an AttributeError later, but I think we could
